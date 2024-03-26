d6eb8e80988c192a5763195f43e3f5946a66aa67
sallyhsieh <sallyhsieh@liontrave.com>
2024-03-26 16:58:40 +0800

    .

--- lib/types/next-auth.d.ts
+++ lib/types/next-auth.d.ts
@@ -1,20 +1,6 @@
 import NextAuth, { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-  /**
-   * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-   */
-  // interface Session {
-  //   user: {
-  //     name?: string | null | undefined;
-  //     email?: string | null | undefined;
-  //     image?: string | null | undefined;
-  //     accessToken: any  & DefaultSession["user"];
-  //   };
-  // }
-  // interface User {
-  //   accessToken: any & DefaultSession["user"];
-  // }
   interface Session {
     user: {
       accessToken: any;
